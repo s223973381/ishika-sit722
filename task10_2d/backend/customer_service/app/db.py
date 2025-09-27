1cec52ab9f219ba8cc187815e2a056ee5274be27
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-09-27 10:18:29 +1000

    Fix DB engine duplication and CI YAML indentation

--- task10_2d/backend/customer_service/app/db.py
+++ task10_2d/backend/customer_service/app/db.py
@@ -26,7 +26,6 @@ else:
     )
     engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
